33fb894a86dc54cc0594fa6b41ed8b5201ba7424
kpznet <kpznet@outlook.com>
2019-01-08 20:06:11 -0800

    Updates

--- KUAServer.py
+++ KUAServer.py
@@ -17,19 +17,15 @@ def AddPropertyVars(idx, parentNode, totVarSets):
         parentNode.add_property(idx, "PropertyDouble"+"_"+str(i), tNodes, ua.VariantType.Double )
         
 
-def AddRootNode(idx, rootNode, baseName, totNodes, totNodesList, totDepth, totPropSets, totPropSetsList, currDepth):
+def AddRootNode(idx, rootNode, baseName, totNodesList, totPropSetsList, currDepth):
     currDepth = currDepth + 1
-    if currDepth <= totDepth:
+    if currDepth <= len(totNodesList):
         nodeCountAtDepth = totNodesList[currDepth-1]
         AddToNodeCount(nodeCountAtDepth)
         for i in range(1, nodeCountAtDepth+1):
-            if currDepth == totDepth:
                 groot = rootNode.add_object(idx, baseName+"_"+ str(currDepth) + "_" + str(i))
                 AddPropertyVars(idx, groot, totPropSetsList[currDepth-1])
-            else:
-                groot = rootNode.add_object(idx, baseName+"_"+ str(currDepth) + "_" + str(i))
-                AddPropertyVars(idx, groot, totPropSetsList[currDepth-1])
-                AddRootNode(idx, groot, baseName, totNodes, totNodesList, totDepth, totPropSets, totPropSetsList, currDepth)
+                AddRootNode(idx, groot, baseName, totNodesList, totPropSetsList, currDepth)
 
 
 def AddToNodeCount(nAdds):
@@ -45,7 +41,8 @@ if __name__ == "__main__":
     import socket    
     hostname = socket.gethostname()    
     IPAddr = socket.gethostbyname(hostname)    
-    IPAddr = "172.16.10.66"
+    #IPAddr = "172.16.10.66"
+    IPAddr = "LocalHost"
     server = opcua.Server()
     server.set_server_name("KUAServer")
     server.set_application_uri("urn:" + socket.gethostname() + ":KUAServer")
@@ -70,21 +67,15 @@ if __name__ == "__main__":
         tDepth = 4
         tPropSets = 1
 
-        tNodeArray = [3,10,4,6]
-        tPropSetsArray = [5, 5, 5, 5]
-
-        print("Building a ",tNodes, " Nodes By ",tDepth, " Depth By ",tPropSets, " Property Sets Config Hierarchy")
+        tNodeArray = [2,8,4,6]
+        tPropSetsArray = [0, 0, 0, 0]
 
         groot = objects.add_object(idx,"Groot")
-        groot.add_variable(idx, "Nodes per branch", tNodes, ua.VariantType.Int16 )
-        groot.add_variable(idx, "Tree depth", tDepth, ua.VariantType.Int16 )
-        groot.add_variable(idx, "Property sets", tPropSets, ua.VariantType.Int16 )
         groot_totalnodes = groot.add_variable(idx, "Total Nodes", tPropSets, ua.VariantType.Int32 )
-        groot_currenttime = groot.add_variable(idx, "CurrentTime", datetime.now(), ua.VariantType.DateTime)
 
         groot_totalnodes.set_data_value(0, ua.VariantType.UInt32)
         print("Building Server Hierarchy, may connect while constructing...")
-        AddRootNode(idx, groot, "PYNode", tNodes, tNodeArray, tDepth, tPropSets, tPropSetsArray, 0)
+        AddRootNode(idx, groot, "PYNode", tNodeArray, tPropSetsArray, 0)
 
         numNodesTotal = groot_totalnodes.get_data_value().Value.Value
         print("Hierarchy Complete : Total Nodes :", numNodesTotal)
@@ -92,7 +83,6 @@ if __name__ == "__main__":
 
         while True:
             time.sleep(1)
-            groot_currenttime.set_data_value(datetime.now(), ua.VariantType.DateTime)
 
     finally:
         #close connection, remove subcsriptions, etc
